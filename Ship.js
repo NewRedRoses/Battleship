f3dc50f11a72bab00d908d9dc28f68f302800985
Mark A. <mark001616@gmail.com>
2024-02-24 12:36:51 -0500

    Add boilerplate for elementary methods

--- Ship.js
+++ Ship.js
@@ -1,3 +1,14 @@
 class Ship {
-  constructor() {}
+  constructor(length, hitCount) {
+    this.length = length;
+    this.hitCount = hitCount;
+    this.isSunk = false;
+  }
+  hit() {
+    // function that increases # of hits
+  }
+  isSunk() {
+    // function that checks if ship is sunk.
+    // determines it based on length and number of hits taken.
+  }
 }
