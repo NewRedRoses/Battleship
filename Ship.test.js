2016162aca71992756108cd67795b2f8a9a35857
Mark A. <mark001616@gmail.com>
2024-02-25 12:11:25 -0500

    Add tests for ship class

--- Ship.test.js
+++ Ship.test.js
@@ -4,3 +4,20 @@ test("Create ship with 10 hits", () => {
   const object = new Ship(2, 10);
   expect(object.hitCount).toBe(10);
 });
+test("ship takes 3 hits", () => {
+  const object = new Ship(2, 10);
+  for (let i = 0; i < 3; i++) {
+    object.hit();
+  }
+  expect(object.hitCount).toBe(13);
+});
+test("ship is NOT sunken", () => {
+  const object = new Ship(10, 1);
+  expect(object.isSunk).toBe(false);
+});
+
+test("ship is sunken", () => {
+  const object = new Ship(10, 1);
+  object.isSunk = true;
+  expect(object.isSunk).toBe(true);
+});
