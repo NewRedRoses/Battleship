8597785268402bb8cec5b871e8783129382644a0
Mark A. <mark001616@gmail.com>
2024-02-25 20:22:46 -0500

    Add tests and simplify logic

--- Ship.js
+++ Ship.js
@@ -1,17 +1,12 @@
 export default class Ship {
-  constructor(length, hitCount) {
+  constructor(length) {
     this.length = length;
-    this.hitCount = hitCount;
-    this.isSunk = false;
+    this.hitCount = 0;
   }
   hit() {
     this.hitCount++;
   }
   isSunk() {
-    // function that checks if ship is sunk.
-    // determines it based on length and number of hits taken.
-    if (this.hitCount > length) {
-      return true;
-    } else return false;
+    return this.hitCount >= this.length;
   }
 }
--- Ship.test.js
+++ Ship.test.js
@@ -1,23 +1,24 @@
 import Ship from "./Ship";
 
 test("Create ship with 10 hits", () => {
-  const object = new Ship(2, 10);
+  const object = new Ship(2);
+  object.hitCount = 10;
   expect(object.hitCount).toBe(10);
 });
 test("ship takes 3 hits", () => {
-  const object = new Ship(2, 10);
+  const object = new Ship(2);
   for (let i = 0; i < 3; i++) {
     object.hit();
   }
-  expect(object.hitCount).toBe(13);
+  expect(object.hitCount).toBe(3);
 });
 test("ship is NOT sunken", () => {
   const object = new Ship(10, 1);
-  expect(object.isSunk).toBe(false);
+  expect(object.isSunk()).toBe(false);
 });
 
 test("ship is sunken", () => {
-  const object = new Ship(10, 1);
+  const object = new Ship(10);
   object.isSunk = true;
   expect(object.isSunk).toBe(true);
 });
